5842fff5265b1bdf499ff6f619c72d6e5a2906e6
Kate <leunovakate@gmail.com>
2026-07-12 21:47:38 +0300

    comlete 3rd hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,24 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
-}
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
